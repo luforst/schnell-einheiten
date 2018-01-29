8ab0fed57f8de55cdf7e6ebae7802da412911ea9
Lukas Forst <lukas.forst@zoho.com>
2018-01-29 14:13:36 +0100

    Move contentdb constant from main html file into external contentdb.js

---
+++ contentdb.js
@@ -0,0 +1,7 @@
+const contentdb = [
+		["Kraft", "F", "1 N"],
+		["Dichte", "&rho;", "1 \\frac{kg}{m^3}"],
+		["Erd- / Fallbeschleunigung", "g", "9,81\\frac{m}{s^2}"]
+		["Beschleunigung", "a", "1 \\frac{m}{s^2}"]
+		];
+
