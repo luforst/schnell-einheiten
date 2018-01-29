906825a92e65dd521a2349504f034e519c0ad3ec
Lukas Forst <lukas.forst@zoho.com>
2018-01-29 14:50:41 +0100

    Extend contentdb

--- contentdb.js
+++ contentdb.js
@@ -1,7 +1,10 @@
 const contentdb = [
+		["Geschwindigkeit", "v", "1 \\frac{m}{s}"],
+		["Druck", "p", "1 \\frac{N}{m^2}"],
+		["Impuls", "\\vec{p}", "\\vec{p} = 1 \\frac{kg \\cdot m}{s}"],
 		["Kraft", "F", "1 N"],
 		["Dichte", "&rho;", "1 \\frac{kg}{m^3}"],
-		["Erd- / Fallbeschleunigung", "g", "9,81\\frac{m}{s^2}"]
-		["Beschleunigung", "a", "1 \\frac{m}{s^2}"]
+		["Erd- / Fallbeschleunigung", "g", "9,81\\frac{m}{s^2}"],
+		["Beschleunigung", "a", "1 \\frac{m}{s^2}"],
 		];
 
